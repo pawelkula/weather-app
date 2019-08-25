a52c34e1e9f4603a7d17896fcf668b2c1f2f1092
Pawel Kula <pawelkula@gmail.com>
2019-08-25 17:58:03 +0200

    Fixing days message, update README

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 const WEATHER_API_BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast/daily?';
-const WEATHER_API_DAYS = 5;
+export const WEATHER_API_DAYS = 5;
 const WEATHER_API_UNITS = 'metric';
 const WEATHER_API_PARAMETERS = `cnt=${WEATHER_API_DAYS}&units=${WEATHER_API_UNITS}&appid=${process.env.REACT_APP_WEATHER_ID}`;
 
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,6 +1,8 @@
+import { WEATHER_API_DAYS } from './api';
+
 export default {
   API_NOT_FOUND: 'Provided city not found.',
   API_ERROR: 'Error in Weather API.',
-  FORECAST_RESULTS: '5-days forecast, ',
+  FORECAST_RESULTS: `${WEATHER_API_DAYS}-days forecast, `,
   SEARCH_PLACEHOLDER: 'Type the city name...',
 };
